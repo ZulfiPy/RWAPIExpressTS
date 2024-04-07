0f3b28df8a8b371bd3a4b5b4edc0834c2d60a4ad
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-07 16:05:14 +0300

    MongoDB connection controller

---
+++ src/controllers/databaseController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import connectDB from "../config/connectDB";
+
+const connectToDB = async (req: Request, res: Response) => {
+    const dbConnection = await connectDB();
+
+    return res.json({ "databaseConnection": dbConnection })
+}
+
+module.exports = {
+    connectToDB,
+}
