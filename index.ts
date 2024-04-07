b65c2c1135f282f8af5c601fce49806e4943e505
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-07 17:40:00 +0300

    ES module imports, connect to the database and /api/tasks/ route configured

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import connectDB from "./src/config/connectDB";
+import dbConn from "./src/config/dbConn";
+
+import databaseRouter from "./src/routes/database";
+import tasksRouter from "./src/routes/api/tasks";
+
 
 dotenv.config();
 
@@ -12,9 +16,12 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // test database connection
-app.use('/database', require('./src/routes/database'));
+app.use('/database', databaseRouter);
+
+// tasks router GET / POST / PUT / DELETE
+app.use('/api/tasks', tasksRouter);
 
 app.listen(port, async () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
-    await connectDB();
+    await dbConn();
 });
