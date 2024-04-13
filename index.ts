f69409248d91fe69bffa4ef4ddec7a88528dd6f0
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-13 14:08:09 +0300

    /register route is being used and json built-in middleware

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import dbConn from "./src/config/dbConn";
 
 import databaseRouter from "./src/routes/database";
 import tasksRouter from "./src/routes/api/tasks";
-
+import registerRouter from "./src/routes/register";
 
 dotenv.config();
 
@@ -15,8 +15,14 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
+// built-in middleware for json
+app.use(express.json())
+
 // test database connection
 app.use('/database', databaseRouter);
+
+// user register router
+app.use('/register', registerRouter);
 
 // tasks router GET / POST / PUT / DELETE
 app.use('/api/tasks', tasksRouter);
