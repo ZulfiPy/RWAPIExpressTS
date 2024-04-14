0c8b6131ea0e5a34c9416d39f7c330204403df66
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-14 14:29:56 +0300

    database route and controller deleted (not used)

--- src/routes/database.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { connectToDB } from "../controllers/databaseController";
-
-const router = express.Router();
-
-router.route('/')
-    .get(connectToDB)
-
-export default router;
