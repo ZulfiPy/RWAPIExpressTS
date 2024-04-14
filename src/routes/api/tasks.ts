d82ac4966a00a13449de1a9b998fcc049ed85f08
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-14 14:33:59 +0300

    /api/tasks protected by custom middleware isAuthorized

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { getTasks } from "../../controllers/tasksController";
+import { isAuthorized } from "../../middleware/isAuthorized"; 
 
 const router = express.Router();
 
+// /api/tasks route protected with custom isAuthorized function 
 router.route('/')
-    .get(getTasks);
+    .get(isAuthorized([2001]), getTasks);
 
 export default router;
