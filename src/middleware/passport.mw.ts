dd2b5ad3b00e938260bdcdc8589a6ce3fa7b45e3
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-14 14:31:48 +0300

    passportJS middleware configuration file

---
+++ src/middleware/passport.mw.ts
@@ -0,0 +1,45 @@
+import { Express, Request, Response, NextFunction } from "express";
+import passport from "passport";
+import passportJWT from "passport-jwt";
+import { VerifiedCallback } from "passport-jwt";
+
+import User from "../model/User";
+
+interface JwtPayload {
+    UserInfo: {
+        username: string;
+        roles: string[],
+    }
+}
+
+interface JwtOptions {
+    jwtFromRequest: passportJWT.JwtFromRequestFunction<any>;
+    secretOrKey: string;
+}
+
+export async function initPassport(app: Express) {
+    const jwtStrategy = passportJWT.Strategy;
+    const ExtractJWT = passportJWT.ExtractJwt;
+
+    const jwtOptions: JwtOptions = {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_SECRET as string,
+    }
+
+    app.use(passport.initialize());
+
+    passport.use(new jwtStrategy(jwtOptions, async (jwtPayload: JwtPayload, done: VerifiedCallback) => {
+        if (jwtPayload) {
+            const { username } = jwtPayload?.UserInfo;
+            try {
+                const user = await User.findOne({ username });
+                if (!user) done(null, false);
+                done(null, jwtPayload.UserInfo);
+            } catch (error) {
+                console.log('some error occured while looking for a user in the dabase', error);
+            }
+        } else {
+            done(null, false);
+        }
+    }));
+}
