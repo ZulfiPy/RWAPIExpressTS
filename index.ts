8010485127c457543f42112a1b6fa4bad7626a9a
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-14 14:30:55 +0300

    passport.js initialized, /api/tasks protected by passport.js

--- index.ts
+++ index.ts
@@ -2,20 +2,29 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import dbConn from "./src/config/dbConn";
 
-import databaseRouter from "./src/routes/database";
 import tasksRouter from "./src/routes/api/tasks";
 import registerRouter from "./src/routes/register";
-import cors from "cors";
+import authRouter from "./src/routes/auth";
 
+import cors from "cors";
 import { corsOptions } from "./src/config/corsOptions";
+
+import passport from "passport";
+import { initPassport } from "./src/middleware/passport.mw";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+// initialize passport
+initPassport(app);
+
 // CORS - Cross-Origin Resource Sharing
 app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
@@ -24,14 +33,12 @@ app.get('/', (req: Request, res: Response) => {
 // built-in middleware for json
 app.use(express.json())
 
-// test database connection
-app.use('/database', databaseRouter);
-
-// user register router
+// routes
 app.use('/register', registerRouter);
+app.use('/auth', authRouter);
 
-// tasks router GET / POST / PUT / DELETE
-app.use('/api/tasks', tasksRouter);
+// API routes protected with passport.js authentication and isAuthorized custom middleware
+app.use('/api/tasks', passport.authenticate('jwt', { session: false }), tasksRouter);
 
 app.listen(port, async () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
