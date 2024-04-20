f33d08cbc2fd68b472614ca45f2dbd5ca4cdf51a
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-20 08:52:17 +0300

    logout controller delivered; clear the cookies; delete refresh token from the database...

---
+++ src/controllers/logoutController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import User from "../model/User";
+
+const signOut = async (req: Request, res: Response) => {
+    if (!req?.cookies?.jwt) {
+        return res.status(404).json({ "message": "There is not JWT cookie found in your cookies. You might be logged out. Refresh the page" });
+    }
+
+    const refreshToken = req.cookies.jwt;
+
+    const foundUserByRefreshToken = await User.findOne({ refreshTokensArray: refreshToken });
+
+    if (!foundUserByRefreshToken) {
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'none', });
+        return res.status(404).json({ "message": "user not found by the JWT stored in the cookies" });
+    }
+
+    foundUserByRefreshToken.refreshTokensArray = foundUserByRefreshToken.refreshTokensArray.filter(rt => rt !== refreshToken);
+    const result = await foundUserByRefreshToken.save();
+    console.log('old refresh token clear out from the database', result);
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'none', });
+
+    return res.status(200).json({ "message": "sign out try" });
+}
+
+export {
+    signOut
+}
