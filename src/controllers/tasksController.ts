5cd990b4ca66c560598e626401c04b5e066ddd83
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-24 08:59:49 +0300

    get tasks and tasks by username refactor

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -7,8 +7,21 @@ const getTasks = async (req: Request, res: Response) => {
     const database = process.env.PG_TASKS_DB as string;
     await pgConn(database);
 
+    const user = req.user as UserAuthPayload;
+    const username = user.username;
+
+    if (!username) {
+        return res.status(401).json({ "message": "user object not provided" });
+    }
+
     try {
         const pool = getPool(database);
+
+        if (!user.roles.includes(5150)) {
+            const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${username}'`);
+
+            return res.status(200).json({ "message": tasks.rowCount === 0 ? "no tasks found. database is empty" : tasks.rows });
+        }
 
         const tasks = await pool.query('SELECT * FROM tasks');
 
@@ -16,7 +29,7 @@ const getTasks = async (req: Request, res: Response) => {
             return res.status(404).json({ "message": "no tasks found. database is empty." });
         }
 
-        return res.status(200).json({ "tasks": tasks.rows });
+        return res.status(200).json({ "tasks": tasks.rowCount === 0 ? "no tasks found. database is empty" : tasks.rows });
     } catch (error) {
         console.log('error occured while reading tasks:', error);
         return res.status(500).json({ "message": "Something went wrong" });
@@ -26,10 +39,6 @@ const getTasks = async (req: Request, res: Response) => {
 const getTasksByUsername = async (req: Request, res: Response) => {
     const database = process.env.PG_TASKS_DB as string;
     await pgConn(database);
-
-    if (!req.user) {
-        return res.status(401).json({ "message": "user object not provided." });
-    }
 
     const user = req.user as UserAuthPayload;
     const username = user.username;
@@ -44,11 +53,11 @@ const getTasksByUsername = async (req: Request, res: Response) => {
         if (!user.roles.includes(5150)) {
             const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${username}'`);
 
-            return res.status(200).json({ "tasks": tasks.rows });
+            return res.status(200).json({ "tasks": tasks.rowCount === 0 ? "no tasks found. database is empty" : tasks.rows });
         }
         const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${req.params.username}'`);
 
-        return res.status(200).json({ "tasks": tasks.rows });
+        return res.status(200).json({ "tasks": tasks.rowCount === 0 ? "no tasks found. database is empty" : tasks.rows });
     } catch (error) {
         console.log('error occured while reading tasks by username:', error);
         return res.status(500).json({ "message": "Something went wrong" });
