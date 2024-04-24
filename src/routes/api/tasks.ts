fc0b27b6f7eb367a7dbf2a850d842017e9902040
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-24 08:35:03 +0300

    two GET routes, but the second one will handle GET route if username is provided in the URL

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getTasks } from "../../controllers/tasksController";
-import { isAuthorized } from "../../middleware/isAuthorized"; 
+import { getTasks, getTasksByUsername } from "../../controllers/tasksController";
+import { isAuthorized } from "../../middleware/isAuthorized";
 
 const router = express.Router();
 
 // /api/tasks route protected with custom isAuthorized function 
-router.route('/')
-    .get(isAuthorized([2001]), getTasks);
+router.get('', isAuthorized([2001]), getTasks);
+router.get('/:username', isAuthorized([2001]), getTasksByUsername);
 
 export default router;
