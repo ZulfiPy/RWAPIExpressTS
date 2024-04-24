95280a2dacff98484014a2ff119c6fe54410d63e
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-24 08:36:06 +0300

    getTasksByUsername controller delivered, provides tasks by role logic

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import pgConn from "../config/psDbConn";
 import { getPool } from "../config/psDbConn";
+import { UserAuthPayload } from "../types/customTypes";
 
 const getTasks = async (req: Request, res: Response) => {
     const database = process.env.PG_TASKS_DB as string;
@@ -15,14 +16,47 @@ const getTasks = async (req: Request, res: Response) => {
             return res.status(404).json({ "message": "no tasks found. database is empty." });
         }
 
-        return res.status(200).json({ "message": tasks.rows });
+        return res.status(200).json({ "tasks": tasks.rows });
     } catch (error) {
         console.log('error occured while reading tasks:', error);
-        return res.sendStatus(500);
+        return res.status(500).json({ "message": "Something went wrong" });
+    }
+}
+
+const getTasksByUsername = async (req: Request, res: Response) => {
+    const database = process.env.PG_TASKS_DB as string;
+    await pgConn(database);
+
+    if (!req.user) {
+        return res.status(401).json({ "message": "user object not provided." });
+    }
+
+    const user = req.user as UserAuthPayload;
+    const username = user.username;
+
+    if (!username) {
+        return res.status(400).json({ "message": "username not provided." });
+    }
+
+    try {
+        const pool = getPool(database);
+
+        if (!user.roles.includes(5150)) {
+            const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${username}'`);
+
+            return res.status(200).json({ "tasks": tasks.rows });
+        }
+        const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${req.params.username}'`);
+
+        return res.status(200).json({ "tasks": tasks.rows });
+    } catch (error) {
+        console.log('error occured while reading tasks by username:', error);
+        return res.status(500).json({ "message": "Something went wrong" });
     }
 }
 
 
 export {
     getTasks,
+    getTasksByUsername,
 }
