a1097c283d46d1657c745a3497d46c28b125217c
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-26 09:08:34 +0300

    post request route configured with controller

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { getTasks, getTasksByUsername } from "../../controllers/tasksController";
+import { getTasks, getTasksByUsername, createTask } from "../../controllers/tasksController";
 import { isAuthorized } from "../../middleware/isAuthorized";
 
 const router = express.Router();
 
 // /api/tasks route protected with custom isAuthorized function 
-router.get('', isAuthorized([2001]), getTasks);
+router.get('/', isAuthorized([2001]), getTasks);
 router.get('/:username', isAuthorized([2001]), getTasksByUsername);
+router.post('/', isAuthorized([2001]), createTask)
 
 export default router;
