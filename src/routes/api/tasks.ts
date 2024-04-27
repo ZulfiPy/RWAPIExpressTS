40421e4c66cd909735cb39881970e1c564c68428
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-27 09:52:55 +0300

    getOneTaskById controller configured with get request

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getTasks, getTasksByUsername, createTask } from "../../controllers/tasksController";
+import { getTasks, getOneTaskById, getTasksByUsername, createTask } from "../../controllers/tasksController";
 import { isAuthorized } from "../../middleware/isAuthorized";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 // /api/tasks route protected with custom isAuthorized function 
 router.get('/', isAuthorized([2001]), getTasks);
 router.get('/:username', isAuthorized([2001]), getTasksByUsername);
+router.get('/:username/:id', isAuthorized([2001]), getOneTaskById);
 router.post('/', isAuthorized([2001]), createTask)
 
 export default router;
