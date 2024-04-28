5f9255b827de06f5a56c6c6e39b6cf0234db2ee0
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-28 08:59:08 +0300

    put route configured with update task controller

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getTasks, getOneTaskById, getTasksByUsername, createTask } from "../../controllers/tasksController";
+import { getTasks, getOneTaskById, getTasksByUsername, createTask, updateTaskById } from "../../controllers/tasksController";
 import { isAuthorized } from "../../middleware/isAuthorized";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', isAuthorized([2001]), getTasks);
 router.get('/:username', isAuthorized([2001]), getTasksByUsername);
 router.get('/:username/:id', isAuthorized([2001]), getOneTaskById);
-router.post('/', isAuthorized([2001]), createTask)
+router.post('/', isAuthorized([2001]), createTask);
+router.put('/', isAuthorized([2001]), updateTaskById);
 
 export default router;
